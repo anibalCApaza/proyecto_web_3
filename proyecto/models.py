733c69a20714a0cffbf431373027cbf1a2cdb692
Dell arca <arcacorini@gmail.com>
2025-06-06 23:25:56 -0400

    Agregada tabla proyecto

--- proyecto/models.py
+++ proyecto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    fecha_ini = models.DateField()
+    fecha_fin = models.DateField()
+    usuario_creado = models.ForeignKey(User, on_delete=models.CASCADE)
