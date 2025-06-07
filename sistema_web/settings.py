19b936139c9daa00c7976e47445b4cee1d1305f6
Dell arca <arcacorini@gmail.com>
2025-06-06 23:47:58 -0400

    Agregado modelo Tarea

--- sistema_web/settings.py
+++ sistema_web/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "proyecto.apps.ProyectoConfig",
+    "tarea.apps.TareaConfig",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
