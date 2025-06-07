6f3a2d5fa370b3a63414a15fca1dd1e0c76419a4
Dell arca <arcacorini@gmail.com>
2025-06-07 00:39:09 -0400

    Acceso a tablas desde panel de administrador

--- proyecto/admin.py
+++ proyecto/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Proyecto, MiembroProyecto
 # Register your models here.
+
+admin.site.register(Proyecto)
+admin.site.register(MiembroProyecto)
