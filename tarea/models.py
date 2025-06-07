5066f7e070aec6c795860eaf1f9d2c69c98ff47d
Dell arca <arcacorini@gmail.com>
2025-06-06 23:55:11 -0400

    Agregado modelo etiqueta

--- tarea/models.py
+++ tarea/models.py
@@ -9,3 +9,10 @@ class Tarea(models.Model):
     creado_en = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=50)
     proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=50)
+    relevancia = models.CharField(max_length=50)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
