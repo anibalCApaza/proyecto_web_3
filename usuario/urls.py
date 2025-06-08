b68d6efb391a66f1983866d39ed61c707eba269d
Dell arca <arcacorini@gmail.com>
2025-06-08 12:40:38 -0400

    Corrección de name en url de usuario

--- usuario/urls.py
+++ usuario/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("registrarse/", views.registrarse, name="registrarse"),
     path("iniciar_sesion/", views.iniciar_sesion, name="iniciar_sesion"),
     path("cerrar_sesion/", views.cerrar_sesion, name="cerrar_sesion"),
-    path("mostrar_usuario/", views.mostrar_usuario, name="cerrar_sesion"),
+    path("mostrar_usuario/", views.mostrar_usuario, name="mostrar_usuario"),
 ]
