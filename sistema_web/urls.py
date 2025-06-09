10302b7ba033e5ebddf255ece73ca78ea624c686
Dell arca <arcacorini@gmail.com>
2025-06-09 03:16:36 -0400

    Agregación de visualización de etiquetas

--- sistema_web/urls.py
+++ sistema_web/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 # ...existing code...
 from django.urls import path, include
 from django.contrib import admin
@@ -24,5 +25,4 @@ urlpatterns = [
     path("proyecto/", include("proyecto.urls")),
     path("tarea/", include("tarea.urls")),
     # ...existing code...
-
 ]
