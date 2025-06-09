10d8378a448d2e4458a86aa30978e95e65788521
wilder <wilderqq@gmail.com>
2025-06-08 21:05:13 -0400

    agrege cosas en tarea

--- tarea/views.py
+++ tarea/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tarea
+from .forms import EtiquetaForm
+from django.contrib.auth.models import User
 
 # Create your views here.
-
+def lista_tareas(request):
+    tareas = Tarea.objects.all()
+    return render(request, 'tarea/listaTareas.html', {'tareas': tareas})
 
 def ver_tarea(request, id):
     return HttpResponse(
@@ -20,3 +25,32 @@ def crear_etiqueta(request):
     return HttpResponse(
         b"Esta vista muestra un formulario en caso de una peticion GET, si es POST valida los datos de la nueva etiqueta y la registra, es importante considerar que entre estos datos debe figurar el id de la tarea al que se agregara esta etiqueta"
     )
+    
+def agregar_etiqueta(request, tarea_id):
+    tarea = Tarea.objects.get(id=tarea_id)
+    if request.method == 'POST':
+        form = EtiquetaForm(request.POST)
+        if form.is_valid(): #valida el formulario
+           etiqueta = form.save(commit=False) #no se guarda por que falta adicionar datos
+           etiqueta.tarea = tarea
+           etiqueta.save()
+           return redirect('/tarea/lista_tareas')
+    else:
+        form = EtiquetaForm()
+    return render(request, 'tarea/agregar_etiqueta.html', {'form': form, 'tarea': tarea})
+
+def asignar_tarea(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    
+    if request.method == 'POST':
+        usuario_id = request.POST.get('usuario_id')
+        
+        if usuario_id:  # Si se seleccionó un usuario
+            usuario = get_object_or_404(User, id=usuario_id)  # ¡Aquí se usa el User importado!
+            tarea.asignado_a = usuario
+        else:  # Si se eligió desasignar
+            tarea.asignado_a = None
+            
+        tarea.save()
+    
+    return redirect('lista_tareas')
